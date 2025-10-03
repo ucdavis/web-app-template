5d9ff8910d7baad6a5584aa27d0b046d28591a63
Scott Kirkland <srkirkland@gmail.com>
2025-10-03 10:01:09 -0700

    tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css' // load tailwind
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import plugin from "@vitejs/plugin-react";
-import fs from "node:fs";
-import path from "node:path";
-import child_process from "node:child_process";
-import { env } from "node:process";
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+import fs from 'node:fs';
+import path from 'node:path';
+import child_process from 'node:child_process';
+import tailwindcss from '@tailwindcss/vite';
+import { env } from 'node:process';
 
 const baseFolder =
-  env.APPDATA !== undefined && env.APPDATA !== ""
+  env.APPDATA !== undefined && env.APPDATA !== ''
     ? `${env.APPDATA}/ASP.NET/https`
     : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "client";
+const certificateName = 'client';
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
@@ -24,42 +25,42 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
   if (
     0 !==
     child_process.spawnSync(
-      "dotnet",
+      'dotnet',
       [
-        "dev-certs",
-        "https",
-        "--export-path",
+        'dev-certs',
+        'https',
+        '--export-path',
         certFilePath,
-        "--format",
-        "Pem",
-        "--no-password",
+        '--format',
+        'Pem',
+        '--no-password',
       ],
-      { stdio: "inherit" }
+      { stdio: 'inherit' }
     ).status
   ) {
-    throw new Error("Could not create certificate.");
+    throw new Error('Could not create certificate.');
   }
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT
   ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
   : env.ASPNETCORE_URLS
-    ? env.ASPNETCORE_URLS.split(";")[0]
-    : "https://localhost:7258";
+    ? env.ASPNETCORE_URLS.split(';')[0]
+    : 'https://localhost:7258';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin()],
+  plugins: [plugin(), tailwindcss()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     open: true,
     port: 5173,
     proxy: {
-      "^/api": {
+      '^/api': {
         secure: false,
         target,
       },
