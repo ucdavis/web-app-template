3f96bcf96816e29c750698d53415c610d962c01f
Scott Kirkland <srkirkland@gmail.com>
2025-10-07 15:26:05 -0700

    store and persist auth files

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
@@ -72,6 +73,13 @@ builder.Services.AddDbContextPool<AppDbContext>(o => o.UseSqlServer(conn, opt =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure data protection (generated keys for auth and such)
+var keysPath = Path.Combine(builder.Environment.ContentRootPath, "..", ".aspnet", "DataProtection-Keys");
+Directory.CreateDirectory(keysPath);
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(keysPath));
 
 var app = builder.Build();
 
