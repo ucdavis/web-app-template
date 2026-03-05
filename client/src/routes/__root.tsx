2b2a5ef4dd62f41f220000779fb907340a2d92b5
Scott Kirkland <srkirkland@gmail.com>
2026-03-06 05:45:44 +0800

    feat(template): add Walter-style GA bootstrap with placeholder ID

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,9 +2,11 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { RouterContext } from '../main.tsx';
+import { AnalyticsListener } from '@/shared/analytics/AnalyticsListener.tsx';
 
 const RootLayout = () => (
   <>
+    <AnalyticsListener />
     <Outlet />
     <ReactQueryDevtools buttonPosition="top-right" />
     <TanStackRouterDevtools position="bottom-right" />
